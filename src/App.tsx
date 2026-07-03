97f8fd6afd9028909f58ab8f61e8ebf4eb219f02
Manish Raje <rajemanish09@gmail.com>
2026-07-03 23:23:58 +0530

    Configure React Router basename for subdirectory deployment

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const ScrollToTop: React.FC = () => {
 const App: React.FC = () => {
   return (
     <ShopProvider>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         {/* Visual elements */}
         <ScrollProgress />
         <ToastContainer />
